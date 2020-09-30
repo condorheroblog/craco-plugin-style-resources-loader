60c145f30c1280cab8720182625d388dedea7b6f
lixinwei <lixinwei@shanshu.ai>
2020-09-30 10:33:26 +0800

    feat: support less|sass|css|scss|stylus processoir

--- README.md
+++ README.md
@@ -1,57 +1,90 @@
-# Craco style-resources-loader
+# [craco-plugin-style-resources-loader](https://github.com/condorheroblog/craco-plugin-style-resources-loader) 🤡
+
+![craco-plugin-style-resources-loader](https://github.com/condorheroblog/craco-plugin-style-resources-loader/blob/master/index.jpg)
+
 
 [![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 
-This is a [craco](https://github.com/sharegate/craco) plugin to add [style-resources-loader](https://www.npmjs.com/package/style-resources-loader) with [create-react-app](https://facebook.github.io/create-react-app/) version >= 2.
+[![craco-plugin-style-resources-loader](https://img.shields.io/badge/craco--plugin--style--resources--loader-v1.0.0-brightgreen)](https://www.npmjs.com/package/craco-plugin-style-resources-loader)
 
-Inspired by: https://github.com/tilap/craco-sass-resources-loader
+
+If you want to automatically import files (for colors, variables, mixins...) in you project, you can use the `craco-plugin-style-resources-loader` . *in fact introduce css global variables is useful in you project*.
+
+
+> This is a [craco](https://github.com/sharegate/craco) plugin to add [Style Resources Loader](https://github.com/yenshih/style-resources-loader) with [create-react-app](https://facebook.github.io/create-react-app/) version >= 2.
+
+Inspired by: [craco-sass-resources-loader](https://github.com/tilap/craco-sass-resources-loader) 、 [style-resources-loader](https://github.com/spacedawwwg/craco-style-resources-loader#readme) and [vue-cli Working with CSS](https://cli.vuejs.org/guide/css.html)
+
+## Introduction
+
+Add style-resources-loader to your react project easier.
 
 ## Installation
 
 ```bash
-$ yarn add -D craco-style-resources-loader
+$ yarn add -D craco-plugin-style-resources-loader
 
 # OR
 
-$ npm install craco-style-resources-loader --save-dev
+$ npm install craco-plugin-style-resources-loader --save-dev
 ```
 
 ## Usage
 
-`craco-style-resources-loader` expect a `patterns` option containing a string or an array of
-string pointing the CSS files your want to load before any CSS/styles file.
+`craco-plugin-style-resources-loader` expect a `resources` option containing a string or an array of
+string pointing the `scss/sass/css/less` files your want to load before any `scss/sass/css/less` file.
+
+
+## Examples
+
+e.g. `use less processor`
+
+If you only use one less file：
 
 ```js
-const stylesResourcesLoader = require('craco-style-resources-loader');
+const cracoPluginStyleResourcesLoader = require('craco-plugin-style-resources-loader');
+const path = require('path');
 
 module.exports = {
-  plugins: [
-    {
-      plugin: stylesResourcesLoader,
-      options: {
-        patterns: './src/variables.css',
-      },
-    },
-  ],
-}
+    plugin: cracoLessResourcesLoader,
+    options: {
+        patterns: path.join(__dirname, 'src/less/common.less'),
+        /* 
+            Please enter supported CSS processor type
+            1. if u use css processor，please type css string
+            2. if u use less processor，please type less string
+            3. if u use sass or scss processor，please type sass or scss string，Choose one of the two
+            4. if u use stylus processor，please type stylus string
+        */
+        styleType: 'less'
+    }
+};
 ```
 
-You can load multiple CSS files too:
+You can load multiple `scss/sass/css/less` resources files too:
 
 ```js
-const stylesResourcesLoader = require('craco-style-resources-loader');
+const cracoPluginStyleResourcesLoader = require('craco-plugin-style-resources-loader');
+const path = require('path');
 
 module.exports = {
-  plugins: [
-    {
-      plugin: stylesResourcesLoader,
-      options: {
+    plugin: cracoLessResourcesLoader,
+    options: {
         patterns: [
-          './src/global-variables.css',
-          './src/theme-variables.css'
-        ],
-      },
-    },
-  ],
-}
+			path.join(__dirname, 'src/less/common.less'),
+			path.join(__dirname, 'src/less/global.less')
+		],
+        /* 
+            Please enter supported CSS processor type
+            1. if u use css processor，please type css string
+            2. if u use less processor，please type less string
+            3. if u use sass or scss processor，please type sass or scss string，Choose one of the two
+            4. if u use stylus processor，please type stylus string
+        */
+        styleType: 'less'
+    }
+};
 ```
+
+
+Learn more about the configuration of **options** ，Please click on the link 👉 [style-resources-loader](https://www.npmjs.com/package/style-resources-loader)
--- index.js
+++ index.js
@@ -1,36 +1,69 @@
 module.exports = {
-  overrideWebpackConfig: ({ webpackConfig, pluginOptions }) => {
-    // Check webpack config
-    if (
-      !webpackConfig || 
-      !webpackConfig.module || 
-      !webpackConfig.module.rules ||
-      typeof webpackConfig.module.rules !== 'object'
-    ) {
-      throw new Error('craco-style-resources-loader error: no valid webpackConfig.module.rules');
-    }
+    overrideWebpackConfig: ({ webpackConfig, pluginOptions }) => {
+        // Check webpack config
+        if (
+            !webpackConfig ||
+            !webpackConfig.module ||
+            !webpackConfig.module.rules ||
+            typeof webpackConfig.module.rules !== 'object'
+        ) {
+            throw new Error('craco-sass-resources-loader error: no valid webpackConfig.module.rules');
+        };
 
-    // Add the loader rule where needed
-    const output = {...webpackConfig};
-    Object.keys(output.module.rules).forEach((ruleKey, ruleIndex) => {
-      const rule = output.module.rules[ruleKey];
-      if (Object.prototype.hasOwnProperty.call(rule, 'oneOf')) {
-        rule.oneOf.forEach((oneOf, oneOfIndex) => {
-          if (
-            oneOf.test && oneOf.use &&
-            (`${oneOf.test}`.includes('css') || `${oneOf.test}`.includes('styles'))
-          ) {
-            const options = pluginOptions && pluginOptions.patterns ? {
-              patterns: pluginOptions.patterns,
-            } : {};
-            output.module.rules[ruleIndex].oneOf[oneOfIndex].use.push({
-              loader: 'style-resources-loader',
-              options,
-            })
-          }
-        })
-      }
-    })
-    return output;
-  }
+        // Check variable pluginOptions
+        if ( Object.prototype.toString.call(pluginOptions) !== '[object Object]' ) {
+            throw new Error('craco-sass-resources-loader error: variable pluginOptions should is an object');
+        };
+
+        // Check styleType property
+        const mapStyleType = [ 'stylus', 'css', 'scss', 'sass', 'less' ];
+        const styleType = pluginOptions.styleType;
+        if ( typeof styleType !== 'string' ) {
+            throw new Error('craco-sass-resources-loader error: variable styleType is not a string');
+        } else if (!mapStyleType.includes(styleType)) {
+            throw new Error('craco-sass-resources-loader error: The value of variable styleType can only be 「stylus | css | scss | sass | less」');
+        };
+
+        // Add the loader rule where needed
+        const output = { ...webpackConfig };
+        Object.keys(output.module.rules).forEach((ruleKey, ruleIndex) => {
+            const rule = output.module.rules[ruleKey];
+            if (Object.prototype.hasOwnProperty.call(rule, 'oneOf')) {
+                rule.oneOf.forEach((oneOf, oneOfIndex) => {
+                    if ( oneOf.test && oneOf.use ) {
+                        let useCssProcessor = false;
+
+                        switch ( styleType ) {
+                            case 'scss':
+                            case 'sass':
+                                useCssProcessor = `${oneOf.test}`.includes('scss') || `${oneOf.test}`.includes('sass');
+                                break;
+                            case 'css':
+                                useCssProcessor = `${oneOf.test}`.includes('css') || `${oneOf.test}`.includes('style');
+                                break;
+                            default:
+                                useCssProcessor = `${oneOf.test}`.includes(styleType);
+                                break;
+                        };
+
+                        if ( useCssProcessor ) {
+                            const options = {};
+
+                            // more option go link： https://www.npmjs.com/package/style-resources-loader#patterns
+                            pluginOptions.patterns && (options.patterns = pluginOptions.patterns);
+                            pluginOptions.injector && (options.injector = pluginOptions.injector);
+                            pluginOptions.globOptions && (options.globOptions = pluginOptions.globOptions);
+                            pluginOptions.resolveUrl && (options.resolveUrl = pluginOptions.resolveUrl);
+
+                            output.module.rules[ruleIndex].oneOf[oneOfIndex].use.push({
+                                loader: 'style-resources-loader',
+                                options,
+                            });
+                        };
+                    };
+                });
+            };
+        });
+        return output;
+    }
 };
